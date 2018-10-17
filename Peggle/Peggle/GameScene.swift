e263f637e362db20a85a263e74b9e4c3cf9fc159
20063914 <20063914@it103imac11.wit.ie>
2018-10-17 14:49:33 +0100

    Starting Peggle

---
+++ Peggle/Peggle/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  Peggle
+//
+//  Created by 20063914 on 17/10/2018.
+//  Copyright © 2018 Brian McCarthy. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+}
